be61ee02ce6538aa8577d537c02eda14bcc7de76
Szymon Kieloch <szymon@sk-solutions.pl>
2019-08-31 22:22:12 +0200

    Use webcam online classifier
    Changes:
    - Replace the static image with a real-time image classifier from the
        webcam

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
+const webcamElement = document.getElementById('webcam');
+
+async function setupWebcam() {
+  return new Promise((resolve, reject) => {
+    const navigatorAny = navigator;
+
+    navigator.getUserMedia =
+      navigator.getUserMedia ||
+      navigatorAny.webkitGetUserMedia ||
+      navigatorAny.mozGetUserMedia ||
+      navigatorAny.msGetUserMedia;
+
+    if (navigator.getUserMedia) {
+      navigator.getUserMedia(
+        {video: true},
+        stream => {
+          webcamElement.srcObject = stream;
+          webcamElement.addEventListener('loadeddata', () => resolve(), false);
+        },
+        () => reject()
+      );
+    } else {
+      reject();
+    }
+  });
+}
+
 async function app() {
   const net = await mobilenet.load();
 
-  const imgEl = document.getElementById('img');
-  const result = await net.classify(imgEl);
+  await setupWebcam();
 
-  console.log('result: ', result);
+  while (true) {
+    const result = await net.classify(webcamElement);
+
+    document.getElementById('console').innerText = `
+      prediction: ${result[0].className}
+      probability: ${result[0].probability}
+    `;
+
+    await tf.nextFrame();
+  }
 }
 
 app();
